e2eb6944aaec681376f67066db77097d41f96073
Morgan <morgan.gowans2001@gmail.com>
2025-08-15 22:16:12 +0100

    WIP: Nav bar

---
+++ src/data/site.ts
@@ -0,0 +1,6 @@
+export const site = {
+  name: 'Further South',
+  eyebrow: 'Further South', // shown above the title; change or set to '' to hide
+  description: 'UK furry convention',
+  logo: '/images/logo-round.png',
+};
