66af58a375b2d8444498a414a4385a8d33b95b57
Morgan <morgan.gowans2001@gmail.com>
2025-08-20 22:15:49 +0100

    Add location+date to navbar and improve nav button behaviour

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,6 +1,8 @@
 export const site = {
   name: 'Further South',
   eyebrow: 'Further South', // shown above the title; change or set to '' to hide
+  location: 'Some Hotel, Portsmouth',
+  date: 'May 25th',
   description: 'UK furry convention',
   logo: '/favicon.svg',
 };
